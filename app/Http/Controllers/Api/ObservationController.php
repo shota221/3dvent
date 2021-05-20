6f792d6de499284db307ca5862cb146d72b20bdb
root <root@localhost.localdomain>
2021-05-20 16:44:26 +0900

    3D_PRINT_COVID-77 TRI用サンプルAPI（モック）

---
+++ app/Http/Controllers/Api/ObservationController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+use App\Services\Api as Service;
+
+use Illuminate\Http\Request;
+
+use App\Http\Forms\Api as Form;
+
+use App\Exceptions;
+
+class ObservationController extends Controller
+{
+    private $service;
+    
+    function __construct() 
+    {
+        $this->service = new Service\ObservationService;
+    }
+
+    // TODO 以下補完作業
+
+    public function count(Request $request)
+    {
+         $response = $this->service->count();
+
+        return $response;
+    }
+
+    public function patientList(Request $request) 
+    {
+        $response = $this->service->getPatientList();
+
+        return $response;
+    }
+
+
+    public function ventilatorList(Request $request) 
+    {
+        $response = $this->service->getVentilatorList();
+
+        return $response;
+    }
+}
---
+++ app/Services/Api/ObservationService.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services\Api;
+
+use App\Exceptions;
+use App\Models;
+use App\Http\Forms\Api as Form;
+use App\Http\Response as Response;
+use App\Repositories as Repos;
+use App\Services\Support as Support;
+use Illuminate\Support\Facades\Auth;
+use App\Services\Support\Converter;
+use App\Services\Support\DBUtil;
+
+class ObservationService
+{
+    
+    //TODO 以下補完作業
+    public function count() {
+         return json_decode(Converter\ObservationConverter::convertToObservationCount(), true);
+    }
+
+    public function getPatientList() {
+        return json_decode(Converter\ObservationConverter::convertToPatientList(), true);
+   }
+
+   public function getVentilatorList() {
+        return json_decode(Converter\ObservationConverter::convertToVentilatorList(), true);
+    }
+
+
+}
