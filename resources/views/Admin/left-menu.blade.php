55f3d3bda0b6feb60b65f614be131a50fc985183
root <root@localhost.localdomain>
2021-10-13 15:04:32 +0900

    サイドバー修正

--- resources/views/Admin/left-menu.blade.php
+++ resources/views/Admin/left-menu.blade.php
@@ -1,42 +1,45 @@
-<li class="nav-item has-treeview">
+<li class="nav-item">
     <a href="{{ route('admin.organization.index') }}"
         class="nav-link {{ is_current_route('admin.organization.*') ? 'active' : '' }}">
-        <i class="fas fa-users"></i>
+        <i class="far fa-hospital"></i>
         <p>
             @lang('messages.organization_management')
-            <i class="fas fa-angle-left right"></i>
         </p>
     </a>
+</li>
+<li class="nav-item">
     <a href="{{ route('admin.org_admin_user.index') }}"
         class="nav-link {{ is_current_route('admin.org_admin_user.*') ? 'active' : '' }}">
         <i class="fas fa-users"></i>
         <p>
             @lang('messages.organization_admin_user_management')
-            <i class="fas fa-angle-left right"></i>
         </p>
     </a>
+</li>
+<li class="nav-item">
     <a href="{{ route('admin.ventilator.index') }}"
         class="nav-link {{ is_current_route('admin.ventilator.*') ? 'active' : '' }}">
-        <i class="fas fa-users"></i>
+        <i class="far fa-square"></i>
         <p>
             @lang('messages.ventilator_management')
-            <i class="fas fa-angle-left right"></i>
         </p>
     </a>
+</li>
+<li class="nav-item">
     <a href="{{ route('admin.ventilator_value.index') }}"
         class="nav-link {{ is_current_route('admin.ventilator_value.*') ? 'active' : '' }}">
-        <i class="fas fa-users"></i>
+        <i class="far fa-list-alt"></i>
         <p>
             @lang('messages.ventilator_value_management')
-            <i class="fas fa-angle-left right"></i>
         </p>
     </a>
+</li>
+<li class="nav-item">
     <a href="{{ route('admin.patient_value.index') }}"
         class="nav-link {{ is_current_route('admin.patient_value.*') ? 'active' : '' }}">
-        <i class="fas fa-users"></i>
+        <i class="far fa-list-alt"></i>
         <p>
             @lang('messages.patient_value_management')
-            <i class="fas fa-angle-left right"></i>
         </p>
     </a>
 </li>
