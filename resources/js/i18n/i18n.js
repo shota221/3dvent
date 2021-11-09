28825e6e206115a5f6a1db251e743bb5dfd1cf1c
root <root@localhost.localdomain>
2021-11-09 13:20:48 +0900

    3D_PRINT_COVID-173 js i18n 多言語化実装

--- resources/js/i18n/i18n.js
+++ resources/js/i18n/i18n.js
@@ -1,18 +1,23 @@
 ;(function(factory) {
     module.exports = factory( 
-        require('i18next'),
-        require('../i18n/ja.json')
+        require('i18next'), 
+        require('../i18n/ja.json'),
+        require('../i18n/en.json')
     );
-}(function(i18n, langJaJson) {
+}(function(i18n, langJaJson, langEnJson) {
 
     return function(lang) {
         // i18n init
         i18n
             .init({
+                lng: $('html')[0].lang,
                 fallbackLng: lang,
                 resources: {
                     ja: {
                         translation: langJaJson
+                    },
+                    en: {
+                        translation: langEnJson
                     }
                 }
             });
