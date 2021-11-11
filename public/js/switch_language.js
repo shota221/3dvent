bfbcfc4a79808d4222994123151a246eead8600c
root <root@localhost.localdomain>
2021-11-11 17:18:57 +0900

    3D_PRINT_COVID-173 微修正

--- public/js/switch_language.js
+++ public/js/switch_language.js
@@ -6,17 +6,17 @@ $('#nav-lang li').on(
         var successCallback = function (data) {
             
             // クッキーの設定値格納
-            var cookie_key    = data['result']['cookie_key'];
-            var language_code = data['result']['language_code'];
-            var domain        = data['result']['domain'];
-            var path          = data['result']['path'];
-            var max_age       = data['result']['max_age'];
+            var cookieKey    = data['result']['cookie_key'];
+            var languageCode = data['result']['language_code'];
+            var domain       = data['result']['domain'];
+            var path         = data['result']['path'];
+            var maxAge       = data['result']['max_age'];
 
             // クッキー設定
-            var cookie        = cookie_key + '=' + language_code;
+            var cookie        = cookieKey + '=' + languageCode;
             var domainOption  = '; Domain=' + domain;
             var pathOption    = '; Path=' + path;
-            var maxAgeOption  = '; Max-Age=' + max_age;
+            var maxAgeOption  = '; Max-Age=' + maxAge;
             var cookieSetting = cookie + domainOption + pathOption + maxAgeOption;
             
             document.cookie = cookieSetting;
