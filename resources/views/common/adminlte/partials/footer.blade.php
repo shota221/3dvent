778a14647d22e63359eb10745e9a46fe61179f6d
root <root@localhost.localdomain>
2021-11-25 17:18:13 +0900

    フッターのcopyrightを新潟病院に変更

--- resources/views/common/adminlte/partials/footer.blade.php
+++ resources/views/common/adminlte/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer text-sm">
-    <strong>Copyright &copy; <a href=""></a>.</strong>All rights reserved.
+    <strong>Copyright &copy;  独立行政法人国立病院機構　新潟病院.</strong>All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> PROTO
     </div>
